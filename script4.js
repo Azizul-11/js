1ba0d5f26237ebca6b220a769d2e795347ba7f88
md azzizul islam <127038709+Azizul-11@users.noreply.github.com>
2024-07-31 21:17:10 +0530

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,20 @@
+const div= document.querySelector('div');
+function change(){
+    div.style.background="blue"
+}
+
+function OnMouse(){
+    div.style.background="green"
+}
+
+function mouseOut(){
+    div.style.background="yellow"
+}
+
+function changeColor(){
+    div.style.background='black'
+}
+
+function changeC(){
+    div.style.background='gray'
+}
